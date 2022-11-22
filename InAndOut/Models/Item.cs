42173417b1b1877a753a19f560e53dbb885d7411
Developer801010 <kristoffermorris80@gmail.com>
2022-11-22 11:52:33 -0500

    Add project files.

---
+++ InAndOut/Models/Item.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InAndOut.Models
+{
+    public class Item
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Borrower { get; set; }
+
+    }
+}
